e969418481c1b0a78421d5b171d5bd84b31ad1e6
JaeHongAhn <woghd387@kyonggi.ac.kr>
2024-05-25 18:07:59 +0900

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,114 @@
+<!--HTML 코드가 들어감-->
+<template>
+  <h1>영화정보</h1>
+  <div v-for="(movie, i) in data" :key="i" class ="item">
+    <figure>
+      <img :src="`${movie.imgUrl}`" :alt="movie.title">
+    </figure>
+    <div class="info">
+      <h3 class="bg-yellow">{{ movie.title }}</h3>
+      <p>개봉: {{ movie.year }}</p>
+      <p>장르: {{ movie.category }}</p>
+      <button @:click="increaseLike(i)"> 좋아요 </button>
+      <span>{{ movie.like }}</span>
+      <p>
+        <button @click="isModal=true"> 상세 보기 </button>
+      </p>
+    </div>
+  </div>
+
+  <div class="modal" v-if="isModal">
+    <div class="inner">
+      <h3>Detail</h3>
+      <p>영화 상세정보</p>
+      <button @click="isModal=false"> 닫기 </button>
+    </div>
+  </div>
+</template>
+
+<!--자바 스크립트 코드가 들어감-->
+<script>
+// import { data } from './assets/movies.js';
+import data from './assets/movies.js';
+  export default {
+    name: 'App',
+    data() {
+      return {
+        isModal: false,
+        data: data,
+      }
+    },
+    methods: {
+      increaseLike(i) {
+        this.data[i].like += 1;
+      },
+    }
+  }
+</script>
+
+<!--CSS 코드가 들어감-->
+<style scoped>
+  .bg-yellow {
+    background: gold;
+    padding: 10px;
+  }
+
+  * {
+    box-sizing: border-box;
+    margin: 0;
+  }
+
+  h1, h2, h3 {
+    margin-bottom: 1rem;
+  }
+
+  p {
+    margin-bottom: 0.5rem;
+  }
+
+  button {
+    margin-right: 10px;
+    margin-top: 1rem;
+  }
+
+  .item {
+    width: 100%;
+    border: 1px solid #ccc;
+    display: flex;
+    margin-bottom: 20px;
+    padding: 1rem;
+  }
+
+  .item figure {
+    width: 30%;
+    margin-right: 1rem;
+  }
+
+  .item img {
+    width: 100%;
+  }
+
+  .item .info {
+    width: 100%;
+  }
+
+  .modal {
+    background: rgba(0, 0, 0, 0.7);
+    position: fixed;
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .modal .inner {
+    background: #ffffff;
+    width: 80%;
+    padding: 20px;
+    border-radius: 10px;
+  }
+
+</style>
---
+++ src/assets/movies.js
@@ -0,0 +1,27 @@
+const data = [
+    {
+        title: "노량",
+        year: 2023,
+        category: "액션, 드라마",
+        like: 0,
+        imgUrl: "./src/assets/노량.jpg",
+        textRed: "color: red",
+    },
+    {
+        title: "아쿠아맨",
+        year: 2023,
+        category: "액션, 판타지, 어드벤처",
+        like: 0,
+        imgUrl: "./src/assets/아쿠아맨.jpeg",
+    },
+    {
+        title: "재홍맨",
+        year: 2033,
+        category: "액션, 판타지, 어드벤처",
+        like: 0,
+        imgUrl: "./src/assets/재홍맨.jpeg",
+    }
+]
+
+// export { data }
+export default data
